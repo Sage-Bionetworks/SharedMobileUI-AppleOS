02153520c7a0a95ac3c8a70a6c1ab57d5bbcd723
Shannon Young <shannon.young@sagebase.org>
2022-03-31 13:51:31 -0700

    Add default implementation of a radio button toggle

---
+++ Sources/SharedMobileUI/Styles/RadioButtonToggleStyle.swift
@@ -0,0 +1,67 @@
+//
+//  RadioButtonToggleStyle.swift
+//  
+//
+//  Copyright © 2022 Sage Bionetworks. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1.  Redistributions of source code must retain the above copyright notice, this
+// list of conditions and the following disclaimer.
+//
+// 2.  Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation and/or
+// other materials provided with the distribution.
+//
+// 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission. No license is granted to the trademarks of
+// the copyright holders even if such marks are included in this software.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+import SwiftUI
+
+public struct RadioButtonToggleStyle : ToggleStyle {
+    private let dotColor: Color = .hex3E3E3E
+    private let circleSize: CGFloat = 24
+    private let dotSize: CGFloat = 14.4
+    
+    public init() {}
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        Button {
+            configuration.isOn.toggle()
+        } label: {
+            HStack(spacing: 8) {
+                ZStack {
+                    Circle()
+                        .strokeBorder(dotColor)
+                        .frame(width: circleSize, height: circleSize)
+                    Circle()
+                        .fill(dotColor)
+                        .frame(width: dotSize, height: dotSize)
+                        .opacity(configuration.isOn ? 1 : 0)
+                }
+                .padding(.top, 17)
+                .padding(.bottom, 15)
+                .padding(.leading, 24)
+                
+                configuration.label
+            }
+            .background(configuration.isOn ? Color.accentColor : Color.sageWhite)
+        }
+        .buttonStyle(PlainButtonStyle())
+    }
+}
